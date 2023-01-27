0ea774e7afaa665d23b39e70899431d1005c1941
prateek <itzprateek9760@gmail.com>
2023-01-27 23:20:31 +0530

    functionality for serving/displyaing image

--- src/main/java/com/boot/blog/controllers/FileController.java
+++ src/main/java/com/boot/blog/controllers/FileController.java
@@ -1,11 +1,17 @@
 package com.boot.blog.controllers;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.boot.blog.payloads.FileResponse;
 import com.boot.blog.services.FileService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/image")
@@ -42,6 +50,20 @@ public class FileController {
 		}
 		
 		return new ResponseEntity<>(new FileResponse(fileName, "Image Uploaded successfully!"), HttpStatus.OK);
+		
+	}
+	
+	
+	
+	// serve / Display or download Image
+	
+	@GetMapping(value = "/download/{imageName}", produces=MediaType.IMAGE_JPEG_VALUE)
+
+	public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException
+	{
+		InputStream content = this.fileService.getImage(path, imageName);
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(content, response.getOutputStream());
 		
 	}
 
--- src/main/java/com/boot/blog/serviceImplemetation/FileServiceImpl.java
+++ src/main/java/com/boot/blog/serviceImplemetation/FileServiceImpl.java
@@ -1,7 +1,10 @@
 package com.boot.blog.serviceImplemetation;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -32,10 +35,7 @@ public class FileServiceImpl implements FileService{
 		//creating Full path   ex: -> C:user/images/abc.png
 		String filePath = path + File.separator + newFileName;
 		
-		
-		
-				
-				
+			
 		// creating Image Folder if not created
 		File f = new File(path);
 		
@@ -53,6 +53,18 @@ public class FileServiceImpl implements FileService{
 		}
 		
 		return fileName;
+	}
+
+	
+	
+	@Override
+	public InputStream getImage(String path, String fileName) throws FileNotFoundException {
+		
+		String fullPath = path + File.separator + fileName;
+		InputStream is = new FileInputStream(fullPath);
+		
+		
+		return is;
 	}
 
 }
